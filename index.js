c9538bb6d9163d032173ffa9cb272ca74a0ae69f
Jishnu <jishnunbhattathiri@gmail.com>
2023-06-08 23:05:12 +0530

    test

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const handler = async (event) => {
 
   const {req, res} = event
   console.log(req.params);
+  console.log("test do..")
   // health check
   if (req.params.health === "health") {
     res.write(JSON.stringify({success: true, msg: "Health check success"}))
